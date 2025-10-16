e197600843e4d2a958344e08849049f8c44a0369
giangehehe365 <gianggiangnguyen010203@gmail.com>
2025-10-16 15:08:56 +0700

    Add full Lab3 project files

---
+++ src/main/java/NguyenMinhGiang/example/InsecureLogin.java
@@ -0,0 +1,19 @@
+package NguyenMinhGiang.example;
+
+public class InsecureLogin {
+    public static void login(String username, String password) {
+        if (username.equals("admin") && password.equals("123456")) {
+            System.out.println("Login successful");
+        } else {
+            System.out.println("Login failed");
+        }
+    }
+
+    public void printUserInfo(String user) {
+        System.out.println("User: " + user);
+    }
+
+    public void doNothing() {
+        // no operation
+    }
+}
